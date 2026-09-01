7fbe318a2ca46f0833370ad26e19e9f8dd5c22ae
sujal <raisujal021@gmail.com>
2026-09-01 13:03:39 +0530

    Fix MySQL SSL connection

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -10,16 +10,19 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
-
 DB_PORT = os.getenv("DB_PORT", "3306")
 
 DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    f"?ssl=true"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {}
+    }
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -33,5 +36,6 @@ Base = declarative_base()
 try:
     with engine.connect():
         print("Database connected successfully!")
+
 except Exception as e:
     print("Database connection failed:", e)
